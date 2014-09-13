bdc6bf5b909c3362a2700a76131b298ca3e3fc27
TodorVyagov <rambo007@abv.bg>
2014-09-13 08:29:06 +0300

    Raphael JS Homework.

---
+++ 05.RaphaelJS-Homework/1.DrawLogos/script.js
@@ -0,0 +1,53 @@
+﻿window.onload = function () {
+
+
+    var paper = Raphael(10, 10, 800, 600);
+
+    // Telerik
+    var telerikLogo = paper.path('M15 20 L25 10 L55 40 L40 55 L25 40 L55 10 L65 20');
+    telerikLogo.attr({
+        stroke: 'yellowgreen',
+        'stroke-width': 8
+    });
+
+    var telerikText = paper.text(150, 35, 'Telerik');
+    telerikText.attr({
+        'font-family': 'Arial',
+        'font-size': '50px',
+        'font-weight': 'bold'
+    });
+
+    var tradeMark = paper.text(240, 30, '\u00AE');
+    tradeMark.attr({
+        'font-family': 'Arial',
+        'font-size': '15px',
+        'font-weight': 'bold'
+    });
+
+    var telerikSlogan = paper.text(170, 65, 'Develop experiences');
+    telerikSlogan.attr({
+        'font-family': 'Arial',
+        'font-size': '20px',
+    });
+
+    // YouTube
+    var you = paper.text(400, 35, 'You');
+    you.attr({
+        'font-family': 'Impact',
+        'font-size': '50px',
+    });
+
+    var redRectangle = paper.rect(440, 7, 120, 60, 25);
+    redRectangle.attr({
+        fill: 'red',
+        stroke: 'none'
+    });
+
+    var tube = paper.text(500, 35, 'Tube');
+    tube.attr({
+        'font-family': 'Impact',
+        'font-size': '50px',
+        fill: '#FFFFFF'
+    });
+   
+}
---
+++ 05.RaphaelJS-Homework/2.DrawSpiral/script.js
@@ -0,0 +1,36 @@
+﻿window.onload = function () {
+
+    var paper = Raphael(10, 10, 800, 600),
+        rad = Math.PI / 180,
+        spiralRadius = 200,
+        spiralDensity = 0.1;
+
+    drawSpiral(200, 200, 5, 0, 0);
+
+    function drawSpiral(cx, cy, r, startAngle, endAngle) {
+        var startAngle = startAngle,
+            endAngle = endAngle,
+            currentRadius = r,
+            buffer;
+
+        while (currentRadius < spiralRadius) {
+            arc(cx, cy, currentRadius, startAngle, endAngle);
+
+            currentRadius += spiralDensity;
+            buffer = startAngle;
+            startAngle = endAngle;
+            endAngle = buffer + 5;
+        }
+    }
+
+    function arc(cx, cy, r, startAngle, endAngle) {
+        var x1 = cx + r * Math.cos(-startAngle * rad),
+            x2 = cx + r * Math.cos(-endAngle * rad),
+            y1 = cy + r * Math.sin(-startAngle * rad),
+            y2 = cy + r * Math.sin(-endAngle * rad);
+        paper.path(["M", x1, y1, "A", r, r, 0, +(endAngle - startAngle > 180), 0, x2, y2]);
+    }
+
+    spiralDensity = 0.4;
+    drawSpiral(600, 200, 5, 0, 0);
+}
