1476a826bb22fc30c54a86734f4389f8206fc06f
TodorVyagov <rambo007@abv.bg>
2014-09-13 08:37:22 +0300

    Handlebars homework.

---
+++ 11.Handlebars/2.SelectTemplate/script.js
@@ -0,0 +1,35 @@
+﻿window.onload = function () {
+    var drinks = [{
+        value: 'cola',
+        text: 'Coca-Cola'
+    }, {
+        value: 'fanta',
+        text: 'Fanta'
+    }, {
+        value: 'sprite',
+        text: 'Sprite'
+    }, {
+        value: 'seven-up',
+        text: 'Seven up'
+    }, {
+        value: 'mirinda',
+        text: 'Mirinda'
+    }, {
+        value: 'pepsi',
+        text: 'Pepsi'
+    }];
+
+    var selectTemplateNode = document.getElementById('select-template');
+    var selectTemplateHtml = selectTemplateNode.innerHTML;
+    var selectTemplate = Handlebars.compile(selectTemplateHtml);
+
+    var button = document.getElementById('select-generator-button');
+    button.addEventListener('click', function () {
+        var selectContainer = document.getElementById('select-container');
+        selectContainer.innerHTML = selectTemplate({
+            items: drinks
+        });
+
+        button.parentNode.removeChild(button);
+    });
+}
